5b3864ba82b96d0b2a64352ff48f38027b74d3c5
Mostafa Mahmoud Fawzy <m3asbhomelkeber2@gmail.com>
2026-09-15 00:25:53 +0300

    Protect all-bookings admin endpoint

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,53 +1,12 @@
 const express = require("express");
-
-const {
-    getAllBookings,
-    getMyBookings,
-    getBookingById,
-    createBooking,
-    deleteBooking
-} = require("../controllers/bookingController");
-
+const { getAllBookings, getMyBookings, getBookingById, createBooking, deleteBooking } = require("../controllers/bookingController");
 const authMiddleware = require("../middleware/authMiddleware");
-
-const {
-    createBookingValidation,
-    bookingIdValidation
-} = require("../middleware/validation");
-
+const authorizeRoles = require("../middleware/roleMiddleware");
+const { createBookingValidation, bookingIdValidation } = require("../middleware/validation");
 const router = express.Router();
-
-router.get(
-    "/",
-    authMiddleware,
-    getAllBookings
-);
-
-router.get(
-    "/my",
-    authMiddleware,
-    getMyBookings
-);
-
-router.get(
-    "/:id",
-    authMiddleware,
-    bookingIdValidation,
-    getBookingById
-);
-
-router.post(
-    "/",
-    authMiddleware,
-    createBookingValidation,
-    createBooking
-);
-
-router.delete(
-    "/:id",
-    authMiddleware,
-    bookingIdValidation,
-    deleteBooking
-);
-
+router.get("/", authMiddleware, authorizeRoles("admin"), getAllBookings);
+router.get("/my", authMiddleware, getMyBookings);
+router.get("/:id", authMiddleware, bookingIdValidation, getBookingById);
+router.post("/", authMiddleware, createBookingValidation, createBooking);
+router.delete("/:id", authMiddleware, bookingIdValidation, deleteBooking);
 module.exports = router;
