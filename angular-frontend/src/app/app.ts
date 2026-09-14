288dec928c9a0edd4494a6cecaee0533435061cb
Mostafa Mahmoud Fawzy <m3asbhomelkeber2@gmail.com>
2026-09-14 23:26:14 +0300

    Enable footer routing in root app

--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, RouterLink, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
