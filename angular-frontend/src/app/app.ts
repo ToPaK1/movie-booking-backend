7278cd9aae8221ea388194e78584d181f791a3e0
Mostafa Mahmoud Fawzy <m3asbhomelkeber2@gmail.com>
2026-09-15 04:25:34 +0300

    Integrate shared footer into app

--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Navbar],
+  imports: [RouterOutlet, RouterLink, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
